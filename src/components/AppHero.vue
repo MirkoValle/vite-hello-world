5903fdd7e34e4c199edd1ccf77d6c159c55d20db
MirkoValle <mirko.valle20@gmail.com>
2024-06-07 19:19:30 +0200

    Added bonus component with jumbotron

---
+++ src/components/AppHero.vue
@@ -0,0 +1,21 @@
+<script></script>
+
+<template>
+
+    <section class="jumbo">
+        <img src="../assets/jumbo.png" alt="jumbotron">
+    </section>
+
+</template>
+
+<style scoped>
+section.jumbo {
+    height: 80%;
+    width: 80%;
+}
+
+img {
+    width: 100%;
+    object-fit: contain;
+}
+</style>
